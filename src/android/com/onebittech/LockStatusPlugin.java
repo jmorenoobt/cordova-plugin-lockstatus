cb8e4654f65dca7d12007124b863a7351b8e6e8f
Jorge Moreno <jmoreno@onebittech.com>
2024-02-19 17:47:18 -0300

    fix: missing import

--- src/android/com/onebittech/LockStatusPlugin.java
+++ src/android/com/onebittech/LockStatusPlugin.java
@@ -6,6 +6,7 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.app.Activity;
 
 import android.app.KeyguardManager;
 
